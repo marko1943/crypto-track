e1ebafba7bc23d93a3e2619b201be182fd8ce9ac
Marko Stojiljkovic <markostojiljkovic1943@gmail.com>
2018-09-15 12:44:17 +0200

    bring back env to eslint. add basic home page container

---
+++ src/containers/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './HomePage.scss';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1>I'm a home page</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
