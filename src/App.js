d9c3862fba9f7b9bd8b5c92251b904a070d77566
Marko Stojiljkovic <markostojiljkovic1943@gmail.com>
2018-09-15 12:46:56 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import HomePage from './containers/HomePage/HomePage';
 
-import './App.css';
-
 class App extends Component {
   render() {
     return <HomePage />;
