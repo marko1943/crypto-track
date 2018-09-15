5efa94b686ffb31f459b6f6e1ea91660908ff517
Marko Stojiljkovic <markostojiljkovic1943@gmail.com>
2018-09-15 12:36:47 +0200

    remove unused eslint config for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-    "jest": true
-  },
   "parser": "babel-eslint",
   "extends": "eslint:recommended",
   "parserOptions": {
