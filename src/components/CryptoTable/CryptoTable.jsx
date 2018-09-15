7dcb128f195edad7cd9d1636251e7ced2aec9eb4
Marko Stojiljkovic <markostojiljkovic1943@gmail.com>
2018-09-15 16:27:31 +0200

    local state and submit button of amount you own

--- src/components/CryptoTable/CryptoTable.jsx
+++ src/components/CryptoTable/CryptoTable.jsx
@@ -5,15 +5,33 @@ import styles from './CryptoTable.scss';
 
 class CryptoTable extends Component {
   state = {
-    cryptoData: {}
+    cryptoData: {},
+    myAmount: undefined
   };
 
   getData = () => {
     new CoinMarketCapService().getTickerData().then(res => {
       let arr = Object.values(res);
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].my_value = 0;
+      }
+
       this.setState({ cryptoData: arr });
     }), err => console.log(err);
   };
+
+  handleChange = e => {
+    this.setState({ myAmount: e.target.value });
+  };
+
+  handleClick(i, event) {
+    event.preventDefault();
+    let arr = this.state.cryptoData;
+
+    arr[i].my_value += Number(this.state.myAmount);
+    this.setState({ cryptoData: arr });
+    event.target.reset();
+  }
 
   componentDidMount() {
     this.getData();
@@ -34,7 +52,7 @@ class CryptoTable extends Component {
               <th>$ value of your coin</th>
             </tr>
             {cryptoData.length > 0 &&
-              cryptoData.map(data =>
+              cryptoData.map((data, i) =>
                 <tr key={data.name}>
                   <td>
                     {data.name}
@@ -47,6 +65,21 @@ class CryptoTable extends Component {
                   </td>
                   <td className={data.quotes.USD.percent_change_24h > 0 ? 'green' : 'red'}>
                     {data.quotes.USD.percent_change_24h}
+                  </td>
+                  <td className="input-row">
+                    <form onSubmit={this.handleClick.bind(this, i)}>
+                      <input
+                        type="number"
+                        value={this.state.myAmout}
+                        onChange={this.handleChange}
+                      />
+                      <button type="submit" disabled={!this.state.myAmount}>
+                        Submit
+                      </button>
+                    </form>
+                  </td>
+                  <td>
+                    {data.my_value}
                   </td>
                 </tr>
               )}
