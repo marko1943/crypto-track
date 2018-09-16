1cdd5c36acfd5ffd8fb9ada6761c01831da88376
Marko Stojiljkovic <markostojiljkovic1943@gmail.com>
2018-09-16 14:17:04 +0200

    add react router and route to /details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Route } from 'react-router';
+
 import HomePage from './pages/HomePage/HomePage';
+import DetailsPage from './pages/DetailsPage/DetailsPage';
 
 class App extends Component {
   render() {
-    return <HomePage />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/details" component={DetailsPage} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
